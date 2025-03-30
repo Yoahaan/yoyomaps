0afbf4fafc8634d2d8a4eb0c6b9ee1c78107f562
Yoahaan <yoahaanhy@gmail.com>
2025-03-30 23:54:58 +0530

    Update name_fetcher.py

--- name_fetcher.py
+++ name_fetcher.py
@@ -4,7 +4,8 @@ from geopy.geocoders import Nominatim
 
 def get_lat_lon(place_name):
     geolocator = Nominatim(user_agent="geo_locator")
-    location = geolocator.geocode(place_name)
+    location = geolocator.geocode(place_name, timeout=10)
+
     
     if location:
         return {"latitude": location.latitude, "longitude": location.longitude}
