a04800452550bfe2c6408f88b7b9d8becb268840
Yoahaan <yoahaanhy@gmail.com>
2025-05-03 15:55:58 +0530

    Update api.py

--- api.py
+++ api.py
@@ -3,6 +3,7 @@ import os
 from flask import Flask, request, jsonify
 from map import compute_route  # Import the core logic
 import requests
+import time
 
 app = Flask(__name__)
 
@@ -12,7 +13,7 @@ def root():
 
 
 # get_route endpoint for route fetching
-@app.route('/get_route', methods=['POST'])
+@app.route('/get_route', methods=['POST'])   #this endpoint is for getting the route
 def get_route_api():
     data = request.get_json()
     source = data.get("source")
@@ -28,8 +29,47 @@ def get_route_api():
 
     return jsonify(result), 200
 
+@app.route('/auto_complete', methods=['GET'])
+def auto_complete():
+    start = time.time()
+    
+    
+    query = request.args.get("q")
+    pune_bbox = "73.7,18.4,74.0,18.7"
+    if not query:
+        return jsonify({"error": "Missing query parameter"}), 400
+
+    try:
+        response = requests.get(
+            "https://nominatim.openstreetmap.org/search",
+            params={"q": query, "format": "json", "limit": 5, "bounded": 1,
+                "viewbox": pune_bbox }, # Restricting the search to Pune},
+            headers={"User-Agent": "yoyo-maps-app"}
+        )
+        data = response.json()
+        suggestions = [
+            {
+                "display_name": item["display_name"],
+                "lat": item["lat"],
+                "lon": item["lon"]
+                
+            }
+            for item in data
+        ]
+        return jsonify(suggestions), 200
+    except Exception as e:
+        return jsonify({"error": "Failed to fetch suggestions"}), 500
+    
+@app.route('/ping')
+def ping():
+    return 'pong - 200 OK', 200
+
+
+
     
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
+
+
     
